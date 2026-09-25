dcc7f4d4d5157caa50123c5ba665b4739740fc3b
Ayush Gupta <ayushgupta.7ag@gmail.com>
2026-09-25 10:22:37 +0530

    Content

---
+++ src/content/siteContent.js
@@ -0,0 +1,18 @@
+export const siteContent = {
+  name: 'Explore with Me',
+  social: {
+    youtube: 'https://youtube.com/@its.explorewithme',
+    instagram: 'https://www.instagram.com/explore_with_me_vlogs',
+  },
+  videos: [
+    { url: 'https://youtu.be/jiO-3RdbVGQ' },
+    { url: 'https://youtu.be/XvvcGnWth7A' },
+    { url: 'https://youtu.be/DAtrMaRw12I' },
+    { url: 'https://youtu.be/bZpDKFRw_xY' },
+    { url: 'https://youtu.be/nqH-Ud87k-E' },
+    { url: 'https://youtu.be/O1E1oE0I_Ao' },
+    { url: 'https://youtu.be/OzXD8qmUOeU' },
+    { url: 'https://youtu.be/SBLilh08v7o' },
+  ],
+  categories: ['Road trips', 'City walks', 'Nature', 'Food', 'Culture'],
+};
